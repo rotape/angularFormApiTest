73499017d934706d0590ef263084197a11e732a2
rotape <roger.correu@gmail.com>
2019-11-27 21:59:46 +0100

    changes

--- src/app/basic-data-form/basic-data-form.component.ts
+++ src/app/basic-data-form/basic-data-form.component.ts
@@ -1,56 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-basic-data-form',
   templateUrl: './basic-data-form.component.html',
   styleUrls: ['./basic-data-form.component.less']
 })
 export class BasicDataFormComponent implements OnInit {
-  user: User;
+  faCoffee = faCoffee;
+  user: User = {
+    id: 0,
+    name: '',
+    surname: '',
+    bornDate: { day: 0, month: 0, year: 1982 },
+    identification: '',
+    document_type: 'DNI',
+    gender: 'man',
+    email: '',
+    confirmEmail: '',
+    password: '',
+    matchingPassword: '',
+    address: ''
+  };
   documents = ['dni', 'passport'];
-  genders = ['man', 'woman', 'noanswer'];
   mustMatchError = false;
   emailError = false;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getUserData();
   }
   checkEmails() {
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (this.user.email !== this.user.confirmEmail || !regex.test(this.user.email)) {
-        this.emailError = true;
-      } else {
-        this.emailError = false;
-      }
+    if (
+      this.user.email !== this.user.confirmEmail ||
+      !regex.test(this.user.email)
+    ) {
+      this.emailError = true;
+    } else {
+      this.emailError = false;
+    }
   }
   checkPasswords() {
-    if(this.user.password !== this.user.matchingPassword) {
+    if (this.user.password !== this.user.matchingPassword) {
       this.mustMatchError = true;
     } else {
-        this.mustMatchError = false;
+      this.mustMatchError = false;
     }
   }
   updateUserPassword() {
-    this.http.put(`https://my-json-server.typicode.com/volkz/technical-form/users/${this.user.id}`,
-    this.user).subscribe((res: User) => {
-      this.user = res;
-    });
+    this.http
+      .put(
+        `https://my-json-server.typicode.com/volkz/technical-form/users/${this.user.id}`,
+        this.user
+      )
+      .subscribe((res: User) => {
+        this.user = res;
+      });
   }
   updateUserData() {
-    this.http.put(`https://my-json-server.typicode.com/volkz/technical-form/users/${this.user.id}`, this.user).subscribe((res: User) => {
-      this.user = res;
-    });
+    this.http
+      .put(
+        `https://my-json-server.typicode.com/volkz/technical-form/users/${this.user.id}`,
+        this.user
+      )
+      .subscribe((res: User) => {
+        this.user = res;
+      });
   }
   getUserData(user?: number) {
     let value: number;
-    user ? value = user : value = 1;
-    this.http.get(`https://my-json-server.typicode.com/volkz/technical-form/users/${value}`).subscribe((res: User) => {
-      this.user = res;
-    });
-
+    user ? (value = user) : (value = 1);
+    this.http
+      .get(
+        `https://my-json-server.typicode.com/volkz/technical-form/users/${value}`
+      )
+      .subscribe((res: User) => {
+        this.user = res;
+      });
   }
-
-
 }
